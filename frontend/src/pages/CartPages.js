fa4ec70d65290746d413b96251025d68948790dc
axelw <wolfs.axelw@gmail.com>
2023-01-28 20:23:41 +0100

    creation design panier

--- frontend/src/pages/CartPages.js
+++ frontend/src/pages/CartPages.js
@@ -1,24 +1,43 @@
-// import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'; 
-// import { addToCart } from '../actions/cartActions';
-// import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import '../styles/cart/cart_page.scss'
 
 function CartPages() {
-    // const articleSlug = useParams()
     const cartItems = useSelector(state => state.cart.cartItems)
-    
 
-    return(
-        <div>
-            {cartItems.map(item => (
-                <div key={item.article}>
-                    <img src={item.image} alt={item.name} />
-                    <Link to={`/article/${item.article}`}>{item.name}</Link>
-                    <div>Quantité: {item.qty}</div>
-                    <div>Prix: {item.price} €</div>
+    return (
+        <div className='container cart'>
+            <div className='cart-items'>
+
+                <div className='left-side'>
+
+                    <div className='table-head'>
+                        <h4>Produit</h4>
+                        <h4>Prix</h4>
+                        <h4>Quantité</h4>
+                    </div>
+
+                    {cartItems.map(item => (
+                        <div className='cart-item' key={item.article}>
+                            <div>
+                                <div>
+                                    <img src={item.image} alt={item.name} />
+
+                                    <div>
+                                        <h4><Link to={`/article/${item.article}`}>{item.name}</Link></h4>
+                                    </div>
+
+                                    <h4>{item.price}€</h4>
+
+                                    <div>
+                                        <span>{item.qty}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            </div>
         </div>
     )
 }
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,32 +1,64 @@
-import { CART_ADD_ITEM, LOAD_CART_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, 
+    LOAD_CART_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
 
+    CART_SAVE_PAYMENT_METHOD,
+
+    CART_CLEAR_ITEMS, 
+
+} from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
-    switch (action.type) {
-        case CART_ADD_ITEM:
-            const item = action.payload
-            const existItem = state.cartItems.find(x => x.article === item.article)
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.article === item.article);
 
-            if (existItem) {
-                return {
-                    ...state,
-                    cartItems: state.cartItems.map(x =>
-                        x.article === existItem.article ? item : x
-                    )
-                }
-            } else {
-                return {
-                    ...state,
-                    cartItems: [...state.cartItems, item]
-                }
-            }
-        case LOAD_CART_ITEM:
-            return {
-                ...state,
-                cartItems: action.payload
-            }
-        
-        default:
-            return state
-    }
-}
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.article === existItem.article ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    case LOAD_CART_ITEM:
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
+
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
+      };
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
+      };
+
+    case CART_CLEAR_ITEMS:
+      return {
+        ...state,
+        cartItems: [],
+      };
+
+    default:
+      return state;
+  }
+};
